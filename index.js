10aba8ae74c247b185a756ff42feeea395284526
Ralph <njugunaralph@gmail.com>
2024-11-28 00:19:07 +0300

    strings review lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Ralph';
+
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting = ' Welcome, ' + currentUser[0] +'!';
